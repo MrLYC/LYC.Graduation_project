f2c22d1c09bf7603af740974d43acdf30361a62d
MrLYC <imyikong@gmail.com>
2014-04-22 16:39:01 +0800

    将矩形标记点放到run里

--- 毕设/程序/run.m
+++ 毕设/程序/run.m
@@ -5,7 +5,11 @@ clc;
 img= imread('rect.bmp');
 img= rgb2gray(img);
 
-img = correct_img(img);
+
+imshow(img);
+dot=ginput();       %取四个点，依次是左上，右上，左下，右下,这里我取的是书的四个角
+
+img = correct_img(img, dot);
 
 figure;
 imshow(uint8(img));
