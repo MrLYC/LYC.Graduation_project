774fcc209f735bc0e81d012d6cd082d524d666bf
MrLYC <imyikong@gmail.com>
2014-04-29 20:43:55 +0800

    增加判断

--- 毕设/程序/run.m
+++ 毕设/程序/run.m
@@ -7,8 +7,10 @@ img = imread('rect.bmp');
 imshow(img);
 dot=ginput();       %取四个点，依次是左上，右上，左下，右下,这里我取的是书的四个角
 
-img = get_border(img);
-img = correct_img(img, dot);
+if length(dot) >= 4
+    img = get_border(img);
+    img = correct_img(img, dot);
 
-figure;
-imshow(uint8(img));
+    figure;
+    imshow(uint8(img));
+end
