5cddb56e6b898165e800577814e97266f8596a7d
MrLYC <imyikong@gmail.com>
2014-04-29 16:33:33 +0800

    提供彩色图像给用户选择点

--- 毕设/程序/run.m
+++ 毕设/程序/run.m
@@ -2,13 +2,12 @@ clear all;
 close all;
 clc;
 
-img= imread('rect.bmp');
-img= rgb2gray(img);
-
+img = imread('rect.bmp');
 
 imshow(img);
 dot=ginput();       %取四个点，依次是左上，右上，左下，右下,这里我取的是书的四个角
 
+img = get_border(img);
 img = correct_img(img, dot);
 
 figure;
