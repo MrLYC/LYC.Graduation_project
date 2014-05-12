a499a1a4f8c6d292096c2f4f6794c7e7b9327957
MrLYC <imyikong@gmail.com>
2014-05-12 10:19:36 +0800

    霍夫变换提取直线演示

--- 毕设/程序/run.m
+++ 毕设/程序/run.m
@@ -2,7 +2,7 @@ clear all;
 close all;
 clc;
 
-img = imread('rect.bmp');
+img = imread('rect.jpg');
 
 imshow(img);
 dot=ginput();       %取四个点，依次是左上，右上，左下，右下,这里我取的是书的四个角
@@ -14,4 +14,13 @@ if length(dot) >= 4
 
     figure;
     imshow(img);
+    hold on;
+    lines = HTLine(img, 4);
+    for k = 1:length(lines) 
+
+    xy = [lines(k).point1 ; lines(k).point2]; 
+
+    plot(xy(:,1), xy(:,2), 'LineWidth', 4, 'Color', [.8 .0 .0]); 
+
+    end 
 end
