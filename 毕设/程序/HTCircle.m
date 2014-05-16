5d057664c6a42b88bd9c21ba19481b1c7b45c51b
MrLYC <imyikong@gmail.com>
2014-05-16 11:00:11 +0800

    这样能够检测出圆,但是参数的意义还不明白

--- 毕设/程序/HTCircle.m
+++ 毕设/程序/HTCircle.m
@@ -43,7 +43,7 @@ for i=1:ecount
         for k=1:size_angle  
             a = round(rows(i)-(r_min+(r-1)*step_r)*cos(k*step_angle));  
             b = round(cols(i)-(r_min+(r-1)*step_r)*sin(k*step_angle));  
-            if(a>0&a<=m&b>0&b<=n)  
+            if(a>0 && a<=m && b>0 && b<=n)  
                 hough_space(a,b,r) = hough_space(a,b,r)+1;  
             end  
         end  
@@ -60,7 +60,7 @@ for i=1:ecount
         par3 = floor(index(k)/(m*n))+1;  
         par2 = floor((index(k)-(par3-1)*(m*n))/m)+1;  
         par1 = index(k)-(par3-1)*(m*n)-(par2-1)*m;  
-        if((rows(i)-par1)^2+(cols(i)-par2)^2<(r_min+(par3-1)*step_r)^2+5&...  
+        if((rows(i)-par1)^2+(cols(i)-par2)^2<(r_min+(par3-1)*step_r)^2+5 && ...  
                 (rows(i)-par1)^2+(cols(i)-par2)^2>(r_min+(par3-1)*step_r)^2-5)  
             hough_circle(rows(i),cols(i)) = 1;  
         end  
@@ -73,6 +73,5 @@ for k=1:length
     par2 = floor((index(k)-(par3-1)*(m*n))/m)+1;  
     par1 = index(k)-(par3-1)*(m*n)-(par2-1)*m;  
     par3 = r_min+(par3-1)*step_r;  
-    fprintf(1,'Center %d %d radius %d\n',par1,par2,par3);  
     para(:,k) = [par1,par2,par3]';  
 end  
--- 毕设/程序/run.m
+++ 毕设/程序/run.m
@@ -2,7 +2,7 @@ clear all;
 close all;
 clc;
 
-img = imread('rect.jpg');
+img = imread('3.jpg');
 
 imshow(img);
 dot=ginput();       %取四个点，依次是左上，右上，左下，右下,这里我取的是书的四个角
@@ -10,10 +10,10 @@ dot=ginput();       %取四个点，依次是左上，右上，左下，右下,这里我取的是书的四个
 if length(dot) >= 4
     img = preprocessing(img);
     img = correct_img(img, dot);
-    img = get_border(img);
+    img = get_border(img, 2);
 
     figure;
-    rect = HTRectangle(img, 10);
+    [hough_space,hough_circle,para] = HTCircle(img,2,0.10,94,188,0.5);
     
     imshow(img);
 end
