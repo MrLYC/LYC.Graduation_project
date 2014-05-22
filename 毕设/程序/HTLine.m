7c7994c31c630a5689ebdedd4f6b811b0cefb411
MrLYC <imyikong@gmail.com>
2014-05-22 09:34:01 +0800

    规范格式

--- 毕设/程序/HTLine.m
+++ 毕设/程序/HTLine.m
@@ -1,7 +1,7 @@
 function [H lines] = HTLine(f, n)
 %ªÙ∑Ú±‰ªªºÏ≤‚÷±œﬂ
 
-[H, theta, rho] = hough(f); 
+[H theta rho] = hough(f); 
 peaks = houghpeaks(H, n);
 lines = houghlines(f, theta, rho, peaks) ;
 
