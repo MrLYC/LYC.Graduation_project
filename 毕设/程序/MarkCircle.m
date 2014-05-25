acb88b700bbd941d30a7c79e2ad851286ca99c30
MrLYC <imyikong@gmail.com>
2014-05-25 13:44:14 +0800

    规整代码

--- 毕设/程序/MarkCircle.m
+++ 毕设/程序/MarkCircle.m
@@ -3,11 +3,11 @@ function [] = MarkCircle(circ)
     M = length(circ);
     for i=1:M
         c = circ{i};
-        plot(c.x0,c.y0,'x','LineWidth',2,'Color','b'); 
         alpha=0:pi/50:2*pi;
-        
         x=c.r*cos(alpha) + c.x0; 
         y=c.r*sin(alpha) + c.y0; 
+        
+        plot(c.x0,c.y0,'x','LineWidth',2,'Color','b'); 
         plot(x,y,'LineWidth',2,'Color','c');
     end
 end
