53d94feeaa62e01985b1f74f7d715b4840cef763
MrLYC <imyikong@gmail.com>
2014-05-26 11:21:17 +0800

    完善提示

--- 毕设/程序/MarkCircle.m
+++ 毕设/程序/MarkCircle.m
@@ -7,6 +7,7 @@ function [] = MarkCircle(circ)
         x=c.r*cos(alpha) + c.x0; 
         y=c.r*sin(alpha) + c.y0; 
         
+        disp(['Clircle{' num2str(i) '}: x0=' num2str(c.x0) ', y0=' num2str(c.y0) ', r0=' num2str(c.r)]);
         plot(c.x0,c.y0,'x','LineWidth',2,'Color','b'); 
         plot(x,y,'LineWidth',2,'Color','c');
     end
--- 毕设/程序/MarkRectangle.m
+++ 毕设/程序/MarkRectangle.m
@@ -5,9 +5,15 @@ function [] = MarkRectangle(rect)
     for i=1:L
         r = rect{i};
         M = length(r);
+        
+        para = ['Rectangle{' num2str(i) '}: '];
         for k=1:M
             p = r{k};
+            
             plot(p.x,p.y,'x','LineWidth',4,'Color','r'); 
+            para = [para '(x' num2str(k) '=' num2str(p.x) ', y' num2str(k) '=' num2str(p.y) ') '];
         end
+        
+        disp(para);
     end
 end
