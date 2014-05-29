5132866372353b1a70f3f0bf9e67b6ed75d4f92e
MrLYC <imyikong@gmail.com>
2014-05-29 21:35:27 +0800

    优化提示

--- 毕设/程序/run.m
+++ 毕设/程序/run.m
@@ -2,7 +2,6 @@ clear all;
 close all;
 clc;
 warning off all;
-
 [filename pathname idx] = uigetfile('*.jpg;*.tif;*.png;*.bmp','Select image files');
 
 if isequal(filename,0)
@@ -54,25 +53,37 @@ end
 
 disp(' ');
 disp('正在进行预处理...');
-img = preprocessing(img);
 figure('NumberTitle', 'off', 'Name', '预处理');
 imshow(img);
+drawnow;
+hold on;
+pause(1);
+img = preprocessing(img);
+imshow(img);
+drawnow;
+hold off;
+pause(1);
 
 disp('正在进行透视变换...');
 img = correct_img(img, dot);
 figure('NumberTitle', 'off', 'Name', '透视变换');
 imshow(img,[]);
+drawnow;
+pause(1);
 
 disp('正在进行提取边缘...');
 img = get_border(img, 3);
 figure('NumberTitle', 'off', 'Name', '提取边缘');
 imshow(img);
+drawnow;
+pause(1);
 
 figure('NumberTitle', 'off', 'Name', '检测结果');
 imshow(img);
 axis on;
 drawnow;
 hold on;
+pause(1);
 
 disp(' ');
 disp('正在进行霍夫变换检测矩形...');
@@ -80,18 +91,26 @@ disp('正在进行霍夫变换检测矩形...');
 disp('检测矩形结果：');
 MarkRectangle(rect);
 disp(' ');
+drawnow;
+pause(1);
 
 disp('正在进行霍夫变换检测圆形...');
 [Hc circ] = HTCircle(img, 0.5, min_r, max_r, pc);
 disp('检测圆形结果：');
 MarkCircle(circ);
 disp(' ');
+drawnow;
+pause(1);
 
 hold off;
 
 disp('正在进行描绘参数空间...');
 ShowHr(Hr,pr);
+drawnow;
+pause(1);
 ShowHc(Hc,pc, min_r);
+drawnow;
+pause(1);
 
 warning on all;
 
